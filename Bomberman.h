2fe6c4af45d4ef6e07c6d59fdfbd7657f3374102
josec <josec@DESKTOP-O08JTNU>
2025-02-26 20:33:18 -0400

    Agregar archivos de proyecto.

---
+++ Bomberman.h
@@ -0,0 +1,29 @@
+#pragma once
+#include<iostream>
+using namespace std;
+class Bomberman
+{
+private:
+	string nombre;
+	int x;
+	int y;
+	int bombas;
+	int alcance;
+	int velocidad;
+	int puntaje;
+	int tiempo;
+	int vidas;
+	int estado;
+	int nivel;
+	int direccion;
+	int energia;
+	int nivel_de_abilidad;
+	int nivelProteccion;
+public:
+	string getNombre() { return nombre; }
+	void setNombre(string_nombre){Nombre=}
+public:
+	//Bomberman();
+	//~Bomberman();
+};
+
---
+++ principal.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+#include"Bomberman.h"
+int main() {
+
+	Bomberman b01;
+
+	cout << "hoal";
+
+	cout << "Hola mundo" << endl;
+	return 0;
+}
